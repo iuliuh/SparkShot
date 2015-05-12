e7e164e0c378e0052f6a40ed10a13774a96012a9
Iuliu Horga <iuliu.horga@arobs.com>
2015-05-13 01:45:28 +0300

    Updates deploy batch file to deploy release binaries.

--- deploy/win/PackageInstaller.bat
+++ deploy/win/PackageInstaller.bat
@@ -1,3 +1,3 @@
-windeployqt packages\com.devtelsoftware.sparkshot\data\SparkShot.exe
+windeployqt --release packages\com.devtelsoftware.sparkshot\data\SparkShot.exe
 
 binarycreator --offline-only -c config/config.xml -p packages SparkShotSetup
